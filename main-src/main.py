d92740ab10551c2d71400773ea690042d5a96a31
farzadoxo <farzadebrahimi.oxo@gmail.com>
2024-08-30 11:54:54 +0330

    💾 Some changes applyed

--- main-src/main.py
+++ main-src/main.py
@@ -1,20 +1,22 @@
 import string
 
 
-class MotrebiLang:
+class MotrebiLanguage:
     
     def translate(word:str):
+        system_letters = ["آ","ا","ع"]
         new_word = ""
         words = word.split(" ")
         for i in words:
             first_leter = i[0]
-            if i[0] != "ا" or "آ" :
+            if i[0] not in system_letters:
+                print(i[0])
                 first_leter = i[0]
                 new_word = i.replace(first_leter,"ا")
                 new_word += new_word.join(first_leter)
                 new_word += new_word.join("و")
                 print(new_word)
-            elif i[0] == "ا" or "آ" or "ع":
+            elif i[0] in system_letters:
                 first_leter = i[0]
                 new_word = i.replace(first_leter,"ش")
                 new_word += new_word.join(first_leter)
@@ -22,4 +24,5 @@ class MotrebiLang:
                 print(new_word)
 
 
-MotrebiLang.translate("سلام علیکم ")
+# test 
+MotrebiLanguage.translate("علیکم")
