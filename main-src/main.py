0bb4ece3a3f100e23ef8b940c3c5ae3964305639
farzad <farzadebrahimi.oxo@gmail.com>
2024-09-08 14:31:33 +0330

    📗 Modifyed (Working better)

--- main-src/main.py
+++ main-src/main.py
@@ -6,23 +6,26 @@ class MotrebiLanguage:
     def translate(word:str):
         system_letters = ["آ","ا","ع"]
         new_word = ""
+        resaultـwords = []
+        resault = ""
         words = word.split(" ")
         for i in words:
-            first_leter = i[0]
             if i[0] not in system_letters:
-                print(i[0])
                 first_leter = i[0]
                 new_word = i.replace(first_leter,"ا")
                 new_word += new_word.join(first_leter)
                 new_word += new_word.join("و")
-                print(new_word)
+                resaultـwords.append(new_word)
             elif i[0] in system_letters:
                 first_leter = i[0]
                 new_word = i.replace(first_leter,"ش")
                 new_word += new_word.join(first_leter)
                 new_word += new_word.join('و')
-                print(new_word)
+                resaultـwords.append(new_word)
+        for j in resaultـwords:
+            resault += f"{j} "
+        print(resault)
 
 
 # test 
-MotrebiLanguage.translate("علیکم")
+MotrebiLanguage.translate("سلام برادر عزیز")
